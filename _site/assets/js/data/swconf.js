660f96bbc9d30855581d29939d851a6e07362964
Gismet Majidov <gismetmajidov@gmail.com>
2024-04-05 23:48:51 +0400

    site

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,40 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1712346103',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+        '/tutorials/',
+      
+
+      
+      
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/misc.min.js',
+        '/assets/js/dist/page.min.js',
+        '/assets/js/dist/post.min.js'
+    ],allowHosts: [
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'cdnjs.cloudflare.com'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
