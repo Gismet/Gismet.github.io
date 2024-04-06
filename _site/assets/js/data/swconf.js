699d0560c118eea1814527ff33c6f58609c22026
Gismet Majidov <gismetmajidov@gmail.com>
2024-04-06 15:18:07 +0400

    update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1712397061',resources: [
+    cacheName: 'chirpy-1712397471',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
